efe4d7d262ac4d22df3a2e7aa0e6ec91f19c2f7b
Holger Albrecht <NpWNOFNT1hSU>
2020-01-05 10:40:20 +0100

    Formatted and fixed bet amount counter

--- bot.js
+++ bot.js
@@ -41,18 +41,15 @@ function onMessageHandler(target, context, message, self) {
   // // Ignore messages from the bot
   // if (self) {
   //   return;
-  // } 
+  // }
 
   // Remove whitespace from chat message
   const messageContent = message.trim();
 
-  if (
-    context.username.trim() === "xxsaltbotxx" &&
-    messageContent.includes("@" + opts.identity.username + " - You have ")
-  ) {
+  if (messageContent.includes("@" + opts.identity.username + " - You have ")) {
     balanceSplit = messageContent.split(" ");
     currentBalance = parseInt(balanceSplit[4]);
-    console.log("Current Balance: " + currentBalance)
+    console.log("Current Balance: " + currentBalance);
   }
 
   /*
@@ -61,66 +58,75 @@ function onMessageHandler(target, context, message, self) {
   #######################################################
   */
 
-  // Handling bet messages
-  if (
-    context.username.trim() === "xxsaltbotxx" &&
-    messageContent.includes("Bet complete")
-  ) {
-    // check if it is the first bet this round
-    if (!bettingStarted.includes("True")) {
-      console.log("Betting Has Started");
-      client.say(target, "!balance");
-      betStartDate = new Date();
-      betStartTime = betStartDate.getTime();
-    }
-    // Mark current betting round as started
-    bettingStarted = "True";
-    bettingEnded = "False";
-
-    // Parses user messages to extract bet details
-    splits = messageContent.split(" ");
-    team = splits[5].substring(0, splits[5].length - 1);
-    amount = parseInt(splits[6].substring(0, splits[6].length - 1));
-    if (team.includes("BLUE")) {
-      totalBlueShrooms += amount;
-      totalBlueBets += 1;
-    } else {
-      totalRedShrooms += amount;
-      totalRedBets += 1;
+  if (context.username.trim() === "xxsaltbotxx") {
+    // read balance from chat if toggled
+    if (
+      messageContent.includes("@" + opts.identity.username + " - You have ")
+    ) {
+      balanceSplit = messageContent.split(" ");
+      currentBalance = parseInt(balanceSplit[4]);
+      console.log("Current Balance: " + currentBalance);
     }
 
-    // Prints total bets and amounts for each team after each bet
-    console.log(
-      "Blue: " + totalBlueShrooms + " Shrooms | " + totalBlueBets + " Bets"
-    );
-    console.log(
-      "Red:  " + totalRedShrooms + " Shrooms | " + totalRedBets + " Bets\n"
-    );
-
-    // Calculate duration of current bet cycle
-    currentDate = new Date();
-    currentTime = currentDate.getTime();
-    betTime = (currentTime - betStartTime);
-    
-    console.log("Bet Timer: " + Math.floor(betTime/1000));
-
-    // Bet if current bet cycle reached 160 seconds
-    if((betTime >= 160000) && (didBet.includes("False"))){
-      underdog = "blue";
-      if(totalBlueShrooms > totalRedShrooms) {
-        underdog = "red";
+    // Handling bet messages from users
+    if (messageContent.includes("Bet complete")) {
+      // check if it is the first bet this round
+      if (!bettingStarted.includes("True")) {
+        console.log("Betting Has Started");
+        client.say(target, "!balance");
+        betStartDate = new Date();
+        betStartTime = betStartDate.getTime();
       }
-      betAmount = Math.floor(currentBalance/10);
-      client.say(target, `!${underdog} ${betAmount}`)
-      console.log(`Bot bet ${betAmount} on ${underdog}`);
-      didBet = "True";
+      // Mark current betting round as started
+      bettingStarted = "True";
+      bettingEnded = "False";
+
+      // Parses user messages to extract bet details
+      splits = messageContent.split(" ");
+      team = splits[5].substring(0, splits[5].length - 1);
+      amount = parseInt(splits[6].substring(0, splits[6].length - 1));
+      if (team.includes("BLUE")) {
+        totalBlueShrooms += amount;
+        totalBlueBets += 1;
+      } else {
+        totalRedShrooms += amount;
+        totalRedBets += 1;
+      }
+
+      // Prints total bets and amounts for each team after each bet
+      console.log(
+        "Blue: " + totalBlueShrooms + " Shrooms | " + totalBlueBets + " Bets"
+      );
+      console.log(
+        "Red:  " + totalRedShrooms + " Shrooms | " + totalRedBets + " Bets\n"
+      );
+
+      // Calculate duration of current bet cycle
+      currentDate = new Date();
+      currentTime = currentDate.getTime();
+      betTime = currentTime - betStartTime;
+
+      console.log("Bet Timer: " + Math.floor(betTime / 1000));
+
+      // Bet if current bet cycle reached 160 seconds
+      if (betTime >= 160000 && didBet.includes("False")) {
+        underdog = "blue";
+        if (totalBlueShrooms > totalRedShrooms) {
+          underdog = "red";
+        }
+        betAmount = Math.floor(currentBalance / 10);
+        client.say(target, `!${underdog} ${betAmount}`);
+        console.log(`Bot bet ${betAmount} on ${underdog}`);
+        didBet = "True";
+      }
     }
   }
 
   // Detect End of Betting Phase
   if (
-    context.username.trim() === "xxsaltbotxx" &&
-    messageContent.includes("Betting has ended") || (betTime > 220000)
+    (context.username.trim() === "xxsaltbotxx" &&
+      messageContent.includes("Betting has ended")) ||
+    betTime > 220000
   ) {
     if (!bettingEnded.includes("True")) {
       console.log("Betting Has Ended!");
@@ -129,9 +135,12 @@ function onMessageHandler(target, context, message, self) {
     bettingEnded = "True";
     bettingStarted = "False";
     betTime = 0;
-    totalRedShrooms, totalBlueShrooms = 0;
+    totalRedShrooms = 0;
+    totalBlueShrooms = 0;
+    totalBlueBets = 0;
+    totalRedBets = 0;
+    didBet = "False";
   }
-
 }
 
 // Called every time the bot connects to Twitch chat
