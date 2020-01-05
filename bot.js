9a2e2a883ed9de461809a509c899c347798e1875
Holger Albrecht <NpWNOFNT1hSU>
2020-01-05 01:26:24 +0100

    Enabled betting for 10 percent of balance

--- bot.js
+++ bot.js
@@ -1,50 +1,140 @@
-const tmi = require('tmi.js');
+const tmi = require("tmi.js");
+var bettingStarted = "False";
+var bettingEnded = "False";
+
+var totalBlueShrooms = 0;
+var totalRedShrooms = 0;
+var totalBlueBets = 0;
+var totalRedBets = 0;
+
+var betTime = 0;
+var betStartDate = 0;
+var betStartTime = 0;
+
+var didBet = "False";
 
 // Define configuration options
 const opts = {
   identity: {
-    username: <BOT_USERNAME>,
-    password: <OAUTH_TOKEN>
+    username: "username",
+    password: "oauth:token"
   },
-  channels: [
-    <CHANNEL_NAME>
-  ]
+  connection: {
+    secure: true,
+    reconnect: true
+  },
+  channels: ["saltyteemo"]
 };
 
 // Create a client with our options
 const client = new tmi.client(opts);
 
 // Register our event handlers (defined below)
-client.on('message', onMessageHandler);
-client.on('connected', onConnectedHandler);
+client.on("message", onMessageHandler);
+client.on("connected", onConnectedHandler);
 
 // Connect to Twitch:
 client.connect();
 
 // Called every time a message comes in
-function onMessageHandler (target, context, msg, self) {
-  if (self) { return; } // Ignore messages from the bot
+function onMessageHandler(target, context, message, self) {
+  // // Ignore messages from the bot
+  // if (self) {
+  //   return;
+  // } 
 
   // Remove whitespace from chat message
-  const commandName = msg.trim();
+  const messageContent = message.trim();
 
-  // If the command is known, let's execute it
-  if (commandName === '!dice') {
-    const num = rollDice();
-    client.say(target, `You rolled a ${num}`);
-    console.log(`* Executed ${commandName} command`);
-  } else {
-    console.log(`* Unknown command ${commandName}`);
+  if (
+    context.username.trim() === "xxsaltbotxx" &&
+    messageContent.includes("@" + opts.identity.username + " - You have ")
+  ) {
+    balanceSplit = messageContent.split(" ");
+    currentBalance = parseInt(balanceSplit[4]);
+    console.log("Current Balance: " + currentBalance)
   }
-}
 
-// Function called when the "dice" command is issued
-function rollDice () {
-  const sides = 6;
-  return Math.floor(Math.random() * sides) + 1;
+  /*
+  #######################################################
+  ###### Handling messages from user "xxsaltbotxx" ######
+  #######################################################
+  */
+
+  // Handling bet messages
+  if (
+    context.username.trim() === "xxsaltbotxx" &&
+    messageContent.includes("Bet complete")
+  ) {
+    // check if it is the first bet this round
+    if (!bettingStarted.includes("True")) {
+      console.log("Betting Has Started");
+      client.say(target, "!balance");
+      betStartDate = new Date();
+      betStartTime = betStartDate.getTime();
+    }
+    // Mark current betting round as started
+    bettingStarted = "True";
+    bettingEnded = "False";
+
+    // Parses user messages to extract bet details
+    splits = messageContent.split(" ");
+    team = splits[5].substring(0, splits[5].length - 1);
+    amount = parseInt(splits[6].substring(0, splits[6].length - 1));
+    if (team.includes("BLUE")) {
+      totalBlueShrooms += amount;
+      totalBlueBets += 1;
+    } else {
+      totalRedShrooms += amount;
+      totalRedBets += 1;
+    }
+
+    // Prints total bets and amounts for each team after each bet
+    console.log(
+      "Blue: " + totalBlueShrooms + " Shrooms | " + totalBlueBets + " Bets"
+    );
+    console.log(
+      "Red:  " + totalRedShrooms + " Shrooms | " + totalRedBets + " Bets\n"
+    );
+
+    // Calculate duration of current bet cycle
+    currentDate = new Date();
+    currentTime = currentDate.getTime();
+    betTime = (currentTime - betStartTime);
+    
+    console.log("Bet Timer: " + Math.floor(betTime/1000));
+
+    // Bet if current bet cycle reached 160 seconds
+    if((betTime >= 160000) && (didBet.includes("False"))){
+      underdog = "blue";
+      if(totalBlueShrooms > totalRedShrooms) {
+        underdog = "red";
+      }
+      betAmount = Math.floor(currentBalance/10);
+      client.say(target, `!${underdog} ${betAmount}`)
+      console.log(`Bot bet ${betAmount} on ${underdog}`);
+      didBet = "True";
+    }
+  }
+
+  // Detect End of Betting Phase
+  if (
+    context.username.trim() === "xxsaltbotxx" &&
+    messageContent.includes("Betting has ended") || (betTime > 220000)
+  ) {
+    if (!bettingEnded.includes("True")) {
+      console.log("Betting Has Ended!");
+    }
+    // Mark current betting round as ended and reset bet cycle variables
+    bettingEnded = "True";
+    bettingStarted = "False";
+    betTime = 0;
+    totalRedShrooms, totalBlueShrooms = 0;
+  }
+
 }
 
 // Called every time the bot connects to Twitch chat
-function onConnectedHandler (addr, port) {
+function onConnectedHandler(addr, port) {
   console.log(`* Connected to ${addr}:${port}`);
 }
